2f2ce901043d837629570d2157de1f29823ec4b4
David Whetstone <david@humblehacker.com>
2022-05-03 22:15:39 -0700

    feat: simplify NavigationLink

--- composable-architecture-android/src/main/kotlin/composablearchitecture/android/NavigationLink.kt
+++ composable-architecture-android/src/main/kotlin/composablearchitecture/android/NavigationLink.kt
@@ -1,16 +1,23 @@
 package composablearchitecture.android
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
+/*
+  Caveat: If you put a NavigationLink with empty content in a Row/Column, it can still
+  affect the layout depending on the arrangement. For example, with SpaceBetween, even though
+  there is no content, space will still be inserted between the empty NavigationLinks.
+
+  Workaround is to place your view's content and the NavigationLinks inside a Box.
+ */
 @Composable
 fun <State, Action> ViewStore<State, Action>.NavigationLink(
     destination: String,
     isActive: Boolean,
     sendIsActive: (Boolean) -> Action,
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit = {}
 ) {
     Column(modifier = Modifier.clickable { send(sendIsActive(true)) }) {
         content()
