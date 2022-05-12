0b7119f3f71a4dd67ba279abd06b50a8e9ea3793
David Whetstone <david@humblehacker.com>
2022-05-11 21:07:25 -0700

    test: Add AlertsAndConfirmationDialogsTests

---
+++ examples/case-studies/src/test/kotlin/composablearchitecture/example/casestudies/jetpackcompose/01-GettingStarted-AlertsAndConfirmationDialogsTests.kt
@@ -0,0 +1,58 @@
+// ktlint-disable filename
+package composablearchitecture.example.casestudies.jetpackcompose
+
+import composablearchitecture.android.Alert
+import composablearchitecture.android.TextState
+import composablearchitecture.test.TestStore
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import org.junit.Ignore
+import org.junit.Test
+
+class AlertsAndConfirmationDialogsTests {
+
+    @Test
+    fun alert() {
+        val dispatcher = TestCoroutineDispatcher()
+        val environment = AlertAndConfirmationDialogEnvironment()
+        val store = TestStore(
+            AlertAndConfirmationDialogState(),
+            alertAndConfirmationDialogReducer,
+            environment,
+            dispatcher
+        )
+
+        store.assert {
+            send(AlertAndConfirmationDialogAction.AlertButtonTapped) {
+                it.copy(
+                    alert = Alert.State(
+                        title = TextState("Alert!"),
+                        message = TextState("This is an alert"),
+                        primaryButton = Alert.Button.cancel(label = TextState("Cancel")),
+                        secondaryButton = Alert.Button.default(
+                            label = TextState("Increment"),
+                            action = Alert.ButtonAction(
+                                Alert.ActionType.Send(
+                                    AlertAndConfirmationDialogAction.IncrementButtonTapped
+                                )
+                            )
+                        )
+                    )
+                )
+            }
+            send(AlertAndConfirmationDialogAction.IncrementButtonTapped) {
+                it.copy(alert = Alert.State(
+                    title = TextState("Incremented!")),
+                    count = 1
+                )
+            }
+            send(AlertAndConfirmationDialogAction.AlertDismissed) {
+                it.copy(alert = null)
+            }
+        }
+    }
+
+    @Test @Ignore
+    fun confirmationDialog() {
+        // TODO
+    }
+}
