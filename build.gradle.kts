efd345a31087c65d8e3f6a068f18a19019252994
David Whetstone <david@humblehacker.com>
2022-08-24 11:08:27 -0700

    chore: add Ben Manes's dependency version checker

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,13 +17,29 @@ allprojects {
         mavenCentral()
         maven { url = uri("https://jitpack.io") }
     }
-
     tasks.withType<KotlinCompile>().all {
         kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlin.ExperimentalStdlibApi"
         kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlinx.coroutines.ExperimentalCoroutinesApi"
         kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlinx.coroutines.FlowPreview"
 
         kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+plugins {
+    id("com.github.ben-manes.versions") version "0.42.0"
+}
+
+tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
+    rejectVersionIf {
+        fun isNonStable(version: String): Boolean {
+            val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+            val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+            val isStable = stableKeyword || regex.matches(version)
+            return isStable.not()
+        }
+
+        isNonStable(candidate.version) && !isNonStable(currentVersion)
     }
 }
 
